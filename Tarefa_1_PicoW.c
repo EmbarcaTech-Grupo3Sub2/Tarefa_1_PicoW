89f377a44d8d90d5f11ec050de73e1d528f2d47b
Luige <luige030301@gmail.com>
2025-01-17 14:22:25 -0300

    Pinos GPIO mapeados e definidos + Ajuste no diagram.json

--- Tarefa_1_PicoW.c
+++ Tarefa_1_PicoW.c
@@ -1,4 +1,19 @@
 #include "pico/stdlib.h"
+#include <stdio.h>
+
+#define RED_LED 13
+#define GREEN_LED 11    // Definição dos LEDs
+#define BLUE_LED 12
+
+const uint col[4] = {4, 3, 2, 1};   // Definição dos pinos do teclado
+const uint rw[4] = {8, 7, 6, 5};    // com as portas GPIO
+
+const char keysMap = {
+    '1', '2', '3', 'A',
+    '4', '5', '6', 'B',
+    '7', '8', '9', 'C',
+    '*', '0', '#', 'D'
+};  
 
 int main(){
     //Primary function
