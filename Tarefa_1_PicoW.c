c7e381b3e285a83215aa74fd453db34764b5c78e
Luinix <luizmarcelolh@gmail.com>
2025-01-18 12:05:29 -0300

    update Identificar tecla Teclado

--- Tarefa_1_PicoW.c
+++ Tarefa_1_PicoW.c
@@ -2,22 +2,87 @@
 #include <stdio.h>
 
 #define RED_LED 13
-#define GREEN_LED 11    // Definição dos LEDs
+#define GREEN_LED 11 // Definição dos LEDs
 #define BLUE_LED 12
 
-#define BUZZER 21    // Definição do buzzer
+#define BUZZER 21 // Definição do buzzer
 
-const uint col[4] = {4, 3, 2, 1};   // Definição dos pinos do teclado
-const uint rw[4] = {8, 7, 6, 5};    // com as portas GPIO
+const uint col[4] = {4, 3, 2, 1}; // Definição dos pinos do teclado
+const uint rw[4] = {8, 7, 6, 5};  // com as portas GPIO
 
-const char keysMap = {
+char lastKey = ' '; // última tecla pressionada
+const char keysMap[16] = {
     '1', '2', '3', 'A',
     '4', '5', '6', 'B',
     '7', '8', '9', 'C',
-    '*', '0', '#', 'D'
-};  
+    '*', '0', '#', 'D'};
 
-int main(){
-    //Primary function
+// Configurar os pinos GPIO para entrada/saída
+void setup_pins()
+{
+    for (int i = 0; i < 4; i++)
+    {
+        // Configura a linha para saida e deixa desativado
+        gpio_init(rw[i]);
+        gpio_set_dir(rw[i], GPIO_OUT);
+        gpio_put(rw[i], 1);
+
+        // Configura a coluna para entrada
+        gpio_init(col[i]);
+        gpio_set_dir(col[i], GPIO_IN);
+        gpio_pull_up(col[i]);
+    }
+}
+
+// Função para verificar qual tecla foi pressionada
+char get_key()
+{
+    char res = ' ';
+    for (int r = 0; r < 4; r++)
+    {
+        gpio_put(rw[r], 0); // Colocar linha atual em estado baixo
+
+        for (int c = 0; c < 4; c++)
+        {
+            sleep_ms(5); // Aguardar 5ms
+            if (gpio_get(col[c]) == 0)
+            {
+                gpio_put(rw[r], 1); // Restaurar linha para estado alto
+                const char key = keysMap[r * 4 + c];
+                res = key; // Retornar o valor da tecla pressionada
+            }
+        }
+        gpio_put(rw[r], 1);
+    }
+    if (res == ' ')
+    {
+        lastKey = res;
+        return res;
+    }
+    else if (res != lastKey)
+    {
+        lastKey = res;
+        return res;
+    }
+    else
+    {
+        return ' ';
+    }
+}
+
+int main()
+{
+    stdio_init_all(); // Para imprimir na serial
+    setup_pins();     // Configurar os pinos do teclado
+
+    while (1)
+    {
+        char key = get_key();
+        if (true || key != ' ')
+        {
+            printf("Tecla: %c\n", key);
+        }
+        sleep_ms(10);
+    }
     return 0;
 }
